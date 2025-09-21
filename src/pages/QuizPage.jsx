271f3bdf1e69fd10202cbc725d73c08874c4dca4
anchit <AnchitSingh>
2025-09-21 10:32:28 +0530

    checkpoint 1.2

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -326,7 +326,6 @@ const QuizPage = ({ onNavigate, quizConfig = null }) => {
             
             <Button
               onClick={isLastQuestion ? confirmStop : nextQuestion}
-              disabled={!hasAnswered}
               icon={
                 <svg className="w-4 h-4 sm:w-5 sm:h-5 ml-1 sm:ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"/>
